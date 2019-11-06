2f955476b28eee2e6ff7eb2096600b35bb7acd51
Ramyres Aquino <ramyres90@gmail.com>
2019-11-06 00:37:57 -0300

    Done exercise 1b 1.3, 1.4 and 1.5

--- part1/courseinfo/src/Content.js
+++ part1/courseinfo/src/Content.js
@@ -4,8 +4,8 @@ import { Part } from './Part';
 
 export const Content = (props) => (
     <div>
-        <Part part={props.parts[0]} exercise={props.exercises[0]} />
-        <Part part={props.parts[1]} exercise={props.exercises[1]} />
-        <Part part={props.parts[2]} exercise={props.exercises[2]} />
+        <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
+        <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
+        <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
     </div>
 );
--- part1/courseinfo/src/Total.js
+++ part1/courseinfo/src/Total.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 export const Total = (props) => {
+    let total = 0;
+    props.parts.forEach(e => { total += e.exercises })
     return (
-        <p>Number of exercises {props.total}</p>
+        <p>Number of exercises {total}</p>
     );
 }
