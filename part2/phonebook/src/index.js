d6b4f270d91649a4c9ee14db0d437799a42551a6
Ramyres Aquino <ramyres90@gmail.com>
2019-11-11 23:27:22 -0300

    Done exercise 2c 2.11 - Phonebook with Axios

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -1,24 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom';
+
+import axios from 'axios';
 
 import Persons from './components/Persons';
 import PersonForm from './components/PersonForm';
 import Filter from './components/Filter';
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ]);
-
+    const [persons, setPersons] = useState([]);
+    const [filter, setFilter] = useState('')
     const [newPerson, setNewPerson] = useState({
+        id: null,
         name: "",
         number: ""
     });
 
-    const [filter, setFilter] = useState('')
+    useEffect(() => {
+        axios
+            .get("http://localhost:3001/persons")
+            .then(result => {
+                setPersons(result.data);
+            })
+    }, [])
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -41,11 +45,11 @@ const App = () => {
     return (
         <div>
             <h2>Phonebook</h2>
-            <Filter filter={filter} onChange={handleSearch}/>
-            
+            <Filter filter={filter} onChange={handleSearch} />
+
             <h3>Add a new</h3>
 
-            <PersonForm handleSubmit={handleSubmit} newPerson={newPerson} setNewPerson={setNewPerson}/>
+            <PersonForm handleSubmit={handleSubmit} newPerson={newPerson} setNewPerson={setNewPerson} />
 
             <h2>Numbers</h2>
             <Persons persons={persons} filter={filter} />
