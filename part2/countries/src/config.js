89f0418e34cb7229af845db4d09e2bd728011e8b
Ramyres Aquino <ramyres90@gmail.com>
2019-11-15 16:15:46 -0300

    Done exercise 2.14 - Implement the weather

---
+++ part2/countries/src/config.js
@@ -0,0 +1,10 @@
+export const weatherstack = {
+    baseURL: 'http://api.weatherstack.com',
+    apiKEY: '43136218ac6ae460dc0cee9b953e5d94'
+}
+
+
+export const restcountries = {
+    baseURL: 'https://restcountries.eu'
+}
+
--- part2/countries/src/index.js
+++ part2/countries/src/index.js
@@ -2,43 +2,79 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
-const CountrieDetail = ({ country }) => (
-    <div>
-        <h1>
-            {country.name}
-        </h1>
-        <p>
-            <b>Capital:</b> {country.capital}
+import { weatherstack, restcountries } from './config';
+
+const WeatherDetail = ({ country }) => {
+    const [weather, setWeather] = useState(null);
+
+    useEffect(()=>{
+        const url = `${weatherstack.baseURL}/current?access_key=${weatherstack.apiKEY}&query=${country.name}`;
+        axios.get(url)
+            .then(result => {
+                console.log(result.data);
+                setWeather(result.data);
+            })
+            .catch(err => {
+                console.error(err);
+
+            });
+    },[country.name])
+
+    return (weather)?(
+        <div>
+            <h3>Weather in {weather.location.country}</h3>
+            <p>
+                <b>Temperature:</b> {weather.current.temperature}° Celsius
         </p>
-        <p>
-            <b>Population:</b> {country.population}
-        </p>
-        <h3>Languages</h3>
-        <ul>
-            {country.languages.map(lang => (
-                <li key={lang.iso639_1}>{lang.name}</li>
-            ))}
-        </ul>
-        <img width={200} src={country.flag} />
-    </div>
-)
+            <img alt={weather.current.weather_descriptions[0]} src={weather.current.weather_icons[0]} />
+            <p>
+                <b>Wind:</b>{weather.current.wind_speed} kph direction {weather.current.wind_dir}
+            </p>
+        </div>
+    ):<br/>
+};
+
+const CountryDetail = ({ country }) => {
+    return (
+        <div>
+            <h1>
+                {country.name}
+            </h1>
+            <p>
+                <b>Capital:</b> {country.capital}
+            </p>
+            <p>
+                <b>Population:</b> {country.population}
+            </p>
+            <h3>Languages</h3>
+            <ul>
+                {country.languages.map(lang => (
+                    <li key={lang.iso639_1}>{lang.name}</li>
+                ))}
+            </ul>
+            <img alt={country.name} width={200} src={country.flag} />
+            <WeatherDetail country={country} />
+        </div>
+    );
+}
 
 const Countries = ({ coutries, showHandler }) => {
-    if (coutries.length == 1)
-        return <CountrieDetail country={coutries[0]} />
+
+    if (coutries.length === 1) {
+        return <CountryDetail country={coutries[0]} />
+    }
 
     return (coutries.length > 10) ?
         <p>To many matches, specify another filter...</p> :
         <div>
             {coutries.map((country) =>
                 <p key={country.alpha2Code}>
-                    {country.name} <button onClick={()=>showHandler(country.name)}>show</button>
+                    {country.name} <button onClick={() => showHandler(country.name)}>show</button>
                 </p>)}
         </div>;
 }
 
 const App = () => {
-    const url = "https://restcountries.eu/rest/v2/all";
     const [filter, setFilter] = useState('bra');
     const [countries, setCountries] = useState([]);
 
@@ -51,6 +87,7 @@ const App = () => {
     }
 
     useEffect(() => {
+        const url = `${restcountries.baseURL}/rest/v2/all`;
         axios
             .get(url)
             .then(result => {
@@ -63,6 +100,9 @@ const App = () => {
             })
     }, [])
 
+    const callBackFilter = (country) => {
+        return country.name.toLowerCase().indexOf(filter.toLowerCase()) >= 0;
+    }
 
     return (
         <div>
@@ -73,9 +113,12 @@ const App = () => {
                 <input id="inputCountries" value={filter} onChange={handleInputChange} autoFocus={true} />
             </div>
             <hr />
-            <Countries coutries={
-                countries.filter((country) => country.name.toLowerCase().indexOf(filter.toLowerCase()) >= 0)
-            } showHandler={showHandler} />
+            <Countries
+                coutries={
+                    countries.filter(callBackFilter)
+                }
+                showHandler={showHandler}
+            />
         </div>
     );
 }
