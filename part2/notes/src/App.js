fd5ce3cf5fc6efd6fb1539dcec6e7c2d2a7f5907
Ramyres Aquino <ramyres90@gmail.com>
2019-11-16 16:50:51 -0300

    Commit after init exercises of part2e - Add style to notes

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 
 import Note from './components/Note';
+import Notification from './components/Notification';
 import noteService from './services/notes';
+import Footer from './components/Footer';
 
 const App = () => {
     const [notes, setNotes] = useState([]);
     const [newNote, setNewNote] = useState('A new note...');
     const [showAll, setShowAll] = useState(true);
+    const [errorMessage, setErrorMessage] = useState(null)
 
     useEffect(() => {
         noteService
@@ -41,7 +44,14 @@ const App = () => {
                 setNotes(notes.map(note => note.id !== id ? note : returnedNote))
             })
             .catch(error => {
-                alert(`the note '${note.content}' was already deleted from server`);
+                setErrorMessage(
+                    `Note '${note.content}' was already removed from server`
+                )
+                
+                setTimeout(() => {
+                    setErrorMessage(null)
+                }, 5000)
+
                 setNotes(notes.filter(n => n.id !== id));
             })
     }
@@ -62,6 +72,7 @@ const App = () => {
     return (
         <div>
             <h1>Notes</h1>
+            <Notification message={errorMessage} />
             <div>
                 <button onClick={() => setShowAll(!showAll)}>
                     show {showAll ? 'important' : 'all'}
@@ -74,6 +85,7 @@ const App = () => {
                 <input value={newNote} onChange={handleNoteChange} />
                 <button type="submit">Save</button>
             </form>
+            <Footer />
         </div>
     );
 }
--- part2/notes/src/components/Note.js
+++ part2/notes/src/components/Note.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Note = ({ note, toggleImportance }) => {
     const label = note.important ? 'Make not important' : 'Make important';
     return (
-        <li>
+        <li className='note'>
             {note.content} <button onClick={toggleImportance}>{label}</button>
         </li>
     )
